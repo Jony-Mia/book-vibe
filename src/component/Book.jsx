1b818db7ded1178c1b729ecce4641907fb66d6b8
Jony-Mia <ajonymia321@gmail.com>
2026-04-09 14:14:49 +0600

    git

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -5,15 +5,17 @@ import { AssetsContext } from '../context/AssetsContext';
 const Book = () => {
     const bookDataList = use(AssetsContext);
     const bookData = use(bookDataList);
+    console.log(bookData);
     
     return (
         <div className='mt-5'>
             <h1 className='text-center font-bold text-4xl '>Books</h1>
-        <div className='grid grid-cols-3 gap-30'>
+        <div className='grid grid-cols-4 gap-3'>
                 {
                 bookData.map(data=><Products
+                    key={data.bookId}
                      id={data.bookId} 
-                     name={data.bookname}
+                     name={data.bookName}
                      autor={data.author} 
                      image ={data.image} 
                      review={data.review} 
@@ -28,22 +30,21 @@ const Book = () => {
     );
 };
 
-export let Products = () => {
+export let Products = ({id,name,autor,image,review,totalPages,ratings,tags,publisher,yearOfPublishing}) => {
 
     return (
         <>
             <div>
-                <div className="card bg-base-100 shadow-sm">
+                <div className="card w-70 bg-base-100 shadow-sm">
                     <figure>
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes" />
+                        <img width={100} src={image} />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Card Title</h2>
-                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+                        <p className="card-title"> {name}</p>
+                        <p>By: {autor}</p>
+                        <div className="card-actions justify-between">
+                            <span>Fiction</span>
+                            <span>5.00 <i className='fas fa-star'></i></span>
                         </div>
                     </div>
                 </div>
